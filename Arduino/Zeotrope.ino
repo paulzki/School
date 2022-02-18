973e890b42e6703d2c3d2d805741474ec1e28af5
Paulzki <81392126+Paulzki@users.noreply.github.com>
2022-02-18 20:40:41 +0100

    Update Zeotrope.ino

--- Arduino/Zeotrope.ino
+++ Arduino/Zeotrope.ino
@@ -1,11 +1,11 @@
-const int controlPin1 = 2;// the control pins will carry the 
+const int controlPin1 = 2;
 logic - direction to turn and applied to the H-Bridge
 const int controlPin2 = 3;
-const int enablePin = 9; // attached to the pin EN
-const int directionSwitchPin = 4;// 4 and 5 carry the values 
+const int enablePin = 9;
+const int directionSwitchPin = 4;
 of buttonSwitches
 const int onOffSwitchStateSwitchPin = 5;
-const int potPin = A5; // analogic signal, because it is a 
+const int potPin = A5; 
 potentiometer delivering continuous values
 int onOffSwitchState = 0;
 int previousOnOffSwitchState = 0;
@@ -23,7 +23,7 @@ void setup() {
   pinMode(enablePin,OUTPUT);
   
 
-  digitalWrite(enablePin,LOW);// the motor initializes at OFF
+  digitalWrite(enablePin,LOW);
 }
 void loop() {
   onOffSwitchState = digitalRead(onOffSwitchStateSwitchPin);
@@ -31,40 +31,35 @@ void loop() {
   directionSwitchState = digitalRead(directionSwitchPin);
   motorSpeed = analogRead(potPin)/4;
 
-  if(onOffSwitchState != previousOnOffSwitchState){ // this 
+  if(onOffSwitchState != previousOnOffSwitchState){ 
 part allows the user to have the motor spin without having to 
 hold the button itself. Otherwise we should keep pressing the 
 button switch to turn the motor.   
-    if(onOffSwitchState ==HIGH){ // if the user presses the 
-button, the state changes. Otherwise, it remains unchanged.
+    if(onOffSwitchState ==HIGH){ 
       motorEnabled = !motorEnabled;
     }
   }
-  if (directionSwitchState != previousDirectionSwitchState) { // analagous to OnOffSwitchState
+  if (directionSwitchState != previousDirectionSwitchState) { 
     if (directionSwitchState == HIGH) {
       motorDirection = !motorDirection;
     }
   }
-  if(motorDirection == 1){ // If the Direction is 1, turn left.
+  if(motorDirection == 1){ 
     digitalWrite(controlPin1, HIGH);
     digitalWrite(controlPin2, LOW);
   }
-  else {  // If the Direction is 0, turn right.
+  else {  
     digitalWrite(controlPin1, LOW);
     digitalWrite(controlPin2, HIGH);
   }
 
-  if(motorEnabled == 1) { // If the motor should be enabled, 
-set EN to HIGH and the motorSpeed indicated by the 
-Potentiometer
+  if(motorEnabled == 1) 
     analogWrite(enablePin, motorSpeed);
   }
   else {
-    analogWrite(enablePin, 0); // If the motor is turned Off, 
-set EN to LOW
+    analogWrite(enablePin, 0); 
   }
   
-  previousDirectionSwitchState = directionSwitchState; // 
-These lines are very important to keep the current state!
+  previousDirectionSwitchState = directionSwitchState;
   previousOnOffSwitchState = onOffSwitchState;
 }
