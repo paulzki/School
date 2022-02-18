31d7c272861b8df3ba736229a69621ba571b7d7c
Paulzki <81392126+Paulzki@users.noreply.github.com>
2022-02-18 20:59:26 +0100

    Update Zeotrope.ino

--- Arduino/Zeotrope.ino
+++ Arduino/Zeotrope.ino
@@ -1,12 +1,9 @@
 const int controlPin1 = 2;
-logic - direction to turn and applied to the H-Bridge
 const int controlPin2 = 3;
 const int enablePin = 9;
 const int directionSwitchPin = 4;
-of buttonSwitches
 const int onOffSwitchStateSwitchPin = 5;
 const int potPin = A5; 
-potentiometer delivering continuous values
 int onOffSwitchState = 0;
 int previousOnOffSwitchState = 0;
 int directionSwitchState = 0;
@@ -32,9 +29,6 @@ void loop() {
   motorSpeed = analogRead(potPin)/4;
 
   if(onOffSwitchState != previousOnOffSwitchState){ 
-part allows the user to have the motor spin without having to 
-hold the button itself. Otherwise we should keep pressing the 
-button switch to turn the motor.   
     if(onOffSwitchState ==HIGH){ 
       motorEnabled = !motorEnabled;
     }
